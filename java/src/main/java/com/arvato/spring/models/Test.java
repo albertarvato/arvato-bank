ac12aa024e839036956ed4c48eb9e1e120c11ec8
Albert <albert.szekely@bertelsmann.de>
2020-07-01 09:26:15 +0300

    switch to postgres

--- java/src/main/java/com/arvato/spring/models/Test.java
+++ java/src/main/java/com/arvato/spring/models/Test.java
@@ -1,9 +1,7 @@
 package com.arvato.spring.models;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
 public class Test {
 
     @Id
