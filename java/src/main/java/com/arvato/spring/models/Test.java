96083dbc23e942dae96a3ff355b36b849f36375f
Albert <albert.szekely@bertelsmann.de>
2020-07-01 10:50:06 +0300

    configure r2dbc

--- java/src/main/java/com/arvato/spring/models/Test.java
+++ java/src/main/java/com/arvato/spring/models/Test.java
@@ -1,35 +1,23 @@
 package com.arvato.spring.models;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("test")
 public class Test {
 
     @Id
-    private String id;
-    private String owner;
-    private Double value;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public Double getValue() {
-        return value;
-    }
-
-    public void setValue(Double value) {
-        this.value = value;
-    }
+    @Column("id")
+    private Integer id;
+    @Column("title")
+    private String title;
+    @Column("content")
+    private String content;
 }
