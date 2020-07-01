61c5e49701cfd9c1a951caa65f07f4ad66a0a8c3
NEAG014 <alexandra.neagu@bertelsmann.de>
2020-07-01 12:12:04 +0300

    created db scripts and models

---
+++ java/src/main/java/com/arvato/spring/models/Transaction.java
@@ -0,0 +1,27 @@
+package com.arvato.spring.models;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("transaction")
+public class Transaction {
+
+    @Id
+    @Column("transaction_id")
+    private Integer transactionId;
+    @Column("created")
+    private OffsetDateTime created;
+    @Column("account_from")
+    private Integer accountFrom;
+    @Column("account_to")
+    private Integer accountTo;
+    @Column("value")
+    private Integer value;
+}
