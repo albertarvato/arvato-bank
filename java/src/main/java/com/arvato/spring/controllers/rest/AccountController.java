0be482402ef80e2a2539e8b9552410560e790574
NEAG014 <alexandra.neagu@bertelsmann.de>
2020-07-03 15:46:27 +0300

    merge with Ion's branch

--- java/src/main/java/com/arvato/spring/controllers/rest/AccountController.java
+++ java/src/main/java/com/arvato/spring/controllers/rest/AccountController.java
@@ -3,8 +3,10 @@ package com.arvato.spring.controllers.rest;
 
 import com.arvato.spring.models.Account;
 import com.arvato.spring.repositories.AccountRepository;
+import com.arvato.spring.repositories.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -21,6 +23,8 @@ class AccountController {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
+    private TransactionRepository transactions;
 
     @PostMapping("")
     public Mono<Account> create(@RequestBody Account account) {
@@ -32,20 +36,24 @@ class AccountController {
     public Mono<Account> get(@PathVariable("id") Integer id) {
         return this.accounts.findById(id);
     }
-
     @PutMapping("/{id}")
-    public Mono<Account> update(@PathVariable("id") Integer id, @RequestBody Account Account) {
+    public Mono<Account> update(@PathVariable("id") Integer id, @RequestBody @NonNull Account account) {
         return this.accounts.findById(id)
                 .map(a -> {
-//                    p.setName(Account.setName());
+                    a.setEmail(account.getEmail());
+                    a.setFullname(account.getFullname() == null ? a.getFullname() : account.getFullname());
+                    a.setIban(account.getIban() == null ? a.getIban() : account.getIban());
+                    a.setMobile(account.getMobile()== null ? a.getMobile() : account.getMobile());
+                    a.setPassword(account.getPassword() == null ? a.getPassword() : account.getPassword());
+                    a.setUsername(account.getUsername() == null ? a.getUsername() : account.getUsername());
                     return a;
                 })
                 .flatMap(a -> this.accounts.save(a));
     }
 
-    @GetMapping("/balance")
-    public Mono<Double> get() {
-        return null;
+    @GetMapping("/{id}/balance")
+    public Mono<Double> getBalance(@PathVariable("id") Integer id) {
+       return transactions.getBalance(id);
     }
 
 }
